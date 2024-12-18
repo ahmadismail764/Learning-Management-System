a6853fca174f94b1226231d66eff40a69a3e2f19
Adelsayed411 <adel15omar1@gmail.com>
2024-12-19 01:28:18 +0200

    Assignment

---
+++ src/main/java/org/software/lms/model/Assignment.java
@@ -0,0 +1,69 @@
+package org.software.lms.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "assignments")
+
+public class Assignment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "due_date")
+    private LocalDate dueDate;
+
+    @Column(name = "course_id", nullable = false) // Foreign key to associate with a course
+    private Long courseId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+}
---
+++ src/main/java/org/software/lms/repository/AssignmentRepository.java
@@ -0,0 +1,10 @@
+package org.software.lms.repository;
+
+
+import org.software.lms.model.Assignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
+}
